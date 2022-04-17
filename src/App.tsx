49a2b8f90b80a4d9769abf65022297f3727f0668
Juan Pablo Saucedo <juan.saucedo@mi.unc.edu.ar>
2022-04-17 11:56:43 -0300

    Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.css';
 import LogInForm from './components/LogInForm';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
+import Folders from './components/Folders';
 
 function App() {
   return (
-    <div className="App">
-      <LogInForm></LogInForm>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LogInForm/>}></Route>
+        <Route path="/home" element={<Folders/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
---
+++ src/components/Folders.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+
+export default class Folders extends Component {
+    render() {
+        return <div>
+            <h1>Folders</h1>
+        </div>
+    }
+}
