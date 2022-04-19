785a597c44782e332aee5c898d8e2da80dc0cce2
Juan Pablo Saucedo <juan.saucedo@mi.unc.edu.ar>
2022-04-18 22:29:00 -0300

    Added styling for FoldersView

--- src/components/FoldersView.tsx
+++ src/components/FoldersView.tsx
@@ -27,72 +27,70 @@ function FoldersView(props: folderViewProps) {
     const handleRemoveFolder = (folderName: string) => (event: any) => {
         event.preventDefault();
         fetch("http://localhost:5001/api/todo/folder",
-        {
-            method: "DELETE",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json,text/*;q=0.99",
-            },
-            body: JSON.stringify({
-                owner: props.username,
-                name: folderName
-            })
-        });
+            {
+                method: "DELETE",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json,text/*;q=0.99",
+                },
+                body: JSON.stringify({
+                    owner: props.username,
+                    name: folderName
+                })
+            });
         fetchFolders(folders.filter((item: any) => item.name !== folderName));
     };
 
     return (
-        <div>
-            <div>
-                <h1>{props.username + "'s Folders"}</h1>
-            </div>
-            <div>
-                {
-                    folders.map((folder: any) =>
-                        <li>
-                            <form method="delete" onSubmit={handleRemoveFolder(folder.name)}>
-                                <p>{folder.name}</p>
-                                <Link to={"/folders/" + folder.name} >
+        <div className="folders-container">
+            <h1 className="folders-header">{props.username + "'s Folders"}</h1>
+            <main className="folders-main">
+                <ul className="folder-list">
+                    {
+                        folders.map((folder: any) =>
+                            <li className="folder-item">
+                                <p className="folder-name">{folder.name}</p>
+                                <Link to={"/folders/" + folder.name} className="folder-view-items" >
                                     View items
                                 </Link>
-                                <button type="submit">Remove</button>
-                            </form>
+                                <p className="folder-remove" onClick={handleRemoveFolder(folder.name)}>Remove</p>
+                            </li>
+                        )
+                    }
+                </ul>
 
-                        </li>
-                    )
-                }
-            </div>
-            <form method="post" onSubmit={(event:any) => {
-                event.preventDefault();
-                const data = {
-                    owner: props.username,
-                    name: newFolder
-                };
-                fetchFolders([...folders, {name: newFolder}]);
-                fetch("http://localhost:5001/api/todo/folder",
-                {
-                    method: "POST",
-                    credentials: "include",
-                    headers: {
-                        "Content-Type": "application/json",
-                        Accept: "application/json,text/*;q=0.99",
-                    },
-                    body: JSON.stringify({
+                <form className="folder-form" method="post" onSubmit={(event: any) => {
+                    event.preventDefault();
+                    const data = {
                         owner: props.username,
                         name: newFolder
-                    })
-                });
-                console.log(folders);
-            }}>
-                <input type="text" 
-                    name="newTask" 
-                    placeholder="New Task"
-                    required 
-                    onChange={e => setNewFolder(e.target.value)}
+                    };
+                    fetchFolders([...folders, { name: newFolder }]);
+                    fetch("http://localhost:5001/api/todo/folder",
+                        {
+                            method: "POST",
+                            credentials: "include",
+                            headers: {
+                                "Content-Type": "application/json",
+                                Accept: "application/json,text/*;q=0.99",
+                            },
+                            body: JSON.stringify({
+                                owner: props.username,
+                                name: newFolder
+                            })
+                        });
+                    console.log(folders);
+                }}>
+                    <input type="text"
+                        name="newFolder"
+                        placeholder="New Folder"
+                        required
+                        onChange={e => setNewFolder(e.target.value)}
                     />
-                <button type="submit">Add</button>
-            </form>
+                    <button type="submit">Add</button>
+                </form>
+            </main>
         </div>
     )
 }
