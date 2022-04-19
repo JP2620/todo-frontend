7f9d733c29504431c25a774c85458c73f148cf78
Juan Pablo Saucedo <juan.saucedo@mi.unc.edu.ar>
2022-04-18 23:42:34 -0300

    Added styling for TasksView

--- src/components/TasksView.tsx
+++ src/components/TasksView.tsx
@@ -7,19 +7,19 @@ interface tasksViewProps extends RouteProps {
 };
 
 function TasksView(props: tasksViewProps) {
-    const [tasks, fetchTasks] = useState<any> ([]);
+    const [tasks, fetchTasks] = useState<any>([]);
     const [newTask, setNewTask] = useState("");
 
     const params = useParams();
     const getTasks = () => {
-        fetch("http://localhost:5001/api/todo/folder/" + params.folder,{
+        fetch("http://localhost:5001/api/todo/folder/" + params.folder, {
             credentials: "include"
         })
-        .then((res) => res.json())
-        .then((res) => {
-            console.log(res);
-            fetchTasks(res);
-        })
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res);
+                fetchTasks(res);
+            })
     }
 
     useEffect(() => {
@@ -27,30 +27,33 @@ function TasksView(props: tasksViewProps) {
     }, []);
 
 
-    
-    
+
+
 
     return (
-        <div>
-            <div>{"Folders > " + params.folder}</div>
-            <ul>
-                {
-                    tasks.map((task: any) => 
-                        <li>
-                            <p>{task.name}</p>
-                            <button>Edit</button>
-                            <input type="checkbox" name={task.name} defaultChecked={task.state === "Completed"} />
-                        </li>
-                    )
-                }
-            </ul>
-            <form method="post" onSubmit={(event: any) => {
+        <div className="tasks-container">
+            <h1 className="tasks-header" style={{fontSize: "24px"}} >{"Folders > " + params.folder}</h1>
+            <main className="tasks-main">
+                <ul className="task-list">
+                    {
+                        tasks.map((task: any) =>
+                            <li className="task-item">
+                                <div className="checkbox-div">
+                                    <input className="task-checkbox" type="checkbox" name={task.name} defaultChecked={task.state === "Completed"} />
+                                </div>
+                                <p className="task-name">{task.name}</p>
+                                <p className="task-edit">Edit</p>
+                            </li>
+                        )
+                    }
+                </ul>
+                <form className="task-form" method="post" onSubmit={(event: any) => {
                     const data = {
                         folder: params.folder,
                         owner: props.username,
                         description: newTask
                     };
-                    fetchTasks([...tasks, {name: newTask}]);
+                    fetchTasks([...tasks, { name: newTask }]);
                     event.preventDefault();
                     fetch("http://localhost:5001/api/todo/task", {
                         method: "POST",
@@ -63,16 +66,18 @@ function TasksView(props: tasksViewProps) {
                     });
                     console.log(tasks);
                 }}>
-                <input type="text" 
-                name="newTask" 
-                placeholder="New Task"
-                required 
-                onChange={e => setNewTask(e.target.value)}
-                />
-                <button type="submit">
-                     Add 
-                </button>
-            </form>
+                    <input type="text"
+                        name="newTask"
+                        placeholder="New Task"
+                        required
+                        onChange={e => setNewTask(e.target.value)}
+                    />
+                    <button type="submit">
+                        Add
+                    </button>
+                </form>
+            </main>
+
         </div>
     )
 }
